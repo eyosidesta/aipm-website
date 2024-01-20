f00452d88696738e6af56a7cf3d6fc08c2b7ab39
eyosidesta <mamesmsdesta@gmail.com>
2024-01-20 16:06:43 -0500

    Grid change

--- aipm_web/src/pages/Users/VissionAndMission.js
+++ aipm_web/src/pages/Users/VissionAndMission.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Grid, makeStyles, Typography, useTheme } from "@material-ui/core";
+import { makeStyles, Typography, useTheme } from "@material-ui/core";
 import { DEEP_BLUE_COLOR, WHITE_COLOR } from "../../utils/constants/colors";
 import backgroundImage from "../../assets/ambaricho_mount.png";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
