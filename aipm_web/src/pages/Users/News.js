58a84d30d6c4280cea901d5bd64189340471dc09
eyosidesta <mamesmsdesta@gmail.com>
2024-01-19 23:25:18 -0500

    fetch news updated

--- aipm_web/src/pages/Users/News.js
+++ aipm_web/src/pages/Users/News.js
@@ -146,15 +146,6 @@ const News = () => {
     },
   ];
   const fetchNews = async () => {
-    // setIsLoading(true);
-    // const res = await getNews();
-    // setIsLoading(false);
-    // dispatch({
-    //     type: "GET_NEWS",
-    //     payload: res
-    // })
-    // dispatch(getNewsAction(res.data.data));
-    // setData(res.data.data.slice(0, 3))
     dispatch(getNewsAction(items));
     setData(items.slice(0, 3))
   };
