9db298f58ab57577c19c05b51029c717fe476846
eyosidesta <mamesmsdesta@gmail.com>
2024-01-22 00:45:33 -0500

    student news added

--- aipm_web/src/components/Admin/UniversityStudents/StudentsList.js
+++ aipm_web/src/components/Admin/UniversityStudents/StudentsList.js
@@ -133,6 +133,20 @@ const StudentsList = () => {
       imageUrl:
         "https://s3.amazonaws.com/media.cloversites.com/e1/e165fffc-7054-48fb-a10c-09c8496cb4f6/site-images/814755c2-b71f-4e73-94b6-6d33885c3a5e@2x.png",
     },
+    {
+      id: 8,
+      title: "Hawassa University Students Movement",
+      detail: `During the 2nd week of October 2021, AIPM held its annual Missions 
+      Conference in Durame. This year we also hosted 100 university students and
+      a few high school students that are very interested in serving the Lord as 
+      missionaries. In total 250 attended the conference. The conference was led by 
+      Dr. Desta and Justin Slemp. Justin is a bord member from AIPM USA. The 
+      following is the report from both Dr. Desta and Justin. During the 2nd week of 
+      October 2021, AIPM held its annual Missions Conference in Durame. This year 
+      we also hosted 100 university students and a few high school students that `,
+      imageUrl:
+        "https://s3.amazonaws.com/media.cloversites.com/e1/e165fffc-7054-48fb-a10c-09c8496cb4f6/site-images/814755c2-b71f-4e73-94b6-6d33885c3a5e@2x.png",
+    },
   ];
 
   const fetchStudentsMovement = async () => {
