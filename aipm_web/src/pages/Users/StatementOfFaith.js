692f0a6808192a459f96ef002be23ab1411e53d8
eyosidesta <mamesmsdesta@gmail.com>
2024-02-14 04:26:45 -0500

    number change

--- aipm_web/src/pages/Users/StatementOfFaith.js
+++ aipm_web/src/pages/Users/StatementOfFaith.js
@@ -94,14 +94,14 @@ const StatementOfFaith = () => {
           Prayer and Mission Movement USA (AIPM USA).
         </Typography>
         <Typography className={classes.listVal}>
-          7. The Ambaricho Prayer and Mission Movement (AIPM)s a Prayer and
+          8. The Ambaricho Prayer and Mission Movement (AIPM)s a Prayer and
           Mission sending organization that is registered as a Non-Profit
           Charitable Organization in Ethiopia. AIPM is also registered in the
           United States as a 501 c3 Charitable Organization as the Ambaricho
           Prayer and Mission Movement USA (AIPM USA).
         </Typography>
         <Typography className={classes.listVal}>
-          7. The Ambaricho Prayer and Mission Movement (AIPM)s a Prayer and
+          9. The Ambaricho Prayer and Mission Movement (AIPM)s a Prayer and
           Mission sending organization that is registered as a Non-Profit
           Charitable Organization in Ethiopia. AIPM is also registered in the
           United States as a 501 c3 Charitable Organization as the Ambaricho
