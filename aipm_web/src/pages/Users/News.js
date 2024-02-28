72841a8c51ce22a421329edf83abdfb5ab150414
eyosidesta <mamesmsdesta@gmail.com>
2024-02-27 23:40:51 -0500

    news updated

--- aipm_web/src/pages/Users/News.js
+++ aipm_web/src/pages/Users/News.js
@@ -79,8 +79,7 @@ const News = () => {
           university students and a few high school students that are very interested in serving the Lord as missionaries. In total 250 
           attended the conference. The conference was led by Dr. Desta and Justin Slemp. Justin is a bord member from AIPM USA. 
           The following is the report from both Dr. Desta and Justin. During the 2nd week of October 2021, AIPM held its annual 
-          Missions Conference in Durame. This year we also hosted 100 university students and a few high school students that are 
-          very interested in serving the Lord as missionaries.`,
+          Missions Conference in Durame.`,
       imageUrl: `${newsImageTwo}`,
     },
     {
@@ -101,8 +100,7 @@ const News = () => {
           attended the conference. The conference was led by Dr. Desta and Justin Slemp. Justin is a bord member from AIPM USA. 
           The following is the report from both Dr. Desta and Justin. During the 2nd week of October 2021, AIPM held its annual 
           Missions Conference in Durame. This year we also hosted 100 university students and a few high school students that are 
-          very interested in serving the Lord as missionaries. In total 250 attended the conference. The conference was led by Dr. Desta 
-          and Justin Slemp. Justin is a bord member from AIPM USA. The following is the report from both Dr. Desta and Justin.`,
+          very interested in serving the Lord as missionaries. In total 250 attended the conference.`,
       imageUrl: `${newsImageFive}`,
     },
     {
