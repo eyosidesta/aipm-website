235de27e32ba31c36fddd8e94edcf72fc48ee4a1
eyosidesta <mamesmsdesta@gmail.com>
2024-02-27 23:36:01 -0500

    year added

--- aipm_web/src/pages/Users/News.js
+++ aipm_web/src/pages/Users/News.js
@@ -108,7 +108,7 @@ const News = () => {
       imageUrl: `${newsImageFive}`,
     },
     {
-      title: "January 20, Ambaricho Mountain Program",
+      title: "January 20, 2024 Ambaricho Mountain Program",
       detail: `During the 2nd week of October 2021, AIPM held its annual Missions Conference in Durame. This year we also hosted 100 
           university students and a few high school students that are very interested in serving the Lord as missionaries. In total 250 
           attended the conference. The conference was led by Dr. Desta and Justin Slemp. Justin is a bord member from AIPM USA. 
