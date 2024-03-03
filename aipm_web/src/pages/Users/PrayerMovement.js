f75bfe8bc2ffddd5862110d4e8aae784899936ae
eyosidesta <mamesmsdesta@gmail.com>
2024-03-03 04:07:07 -0500

    dummy sample data added

--- aipm_web/src/pages/Users/PrayerMovement.js
+++ aipm_web/src/pages/Users/PrayerMovement.js
@@ -155,6 +155,17 @@ const PrayerMovement = () => {
       and Justin Slemp. Justin is a bord member from AIPM USA. The following is the report from both Dr. Desta and Justin.`,
       imageUrl: `${newsImageFive}`,
     },
+    {
+      title: "November 10, 40 days fasting and prayer",
+      detail: `During the 2nd week of October 2021, AIPM held its annual Missions Conference in Durame. This year we also hosted 100 
+      university students and a few high school students that are very interested in serving the Lord as missionaries. In total 250 
+      attended the conference. The conference was led by Dr. Desta and Justin Slemp. Justin is a bord member from AIPM USA. 
+      The following is the report from both Dr. Desta and Justin. During the 2nd week of October 2021, AIPM held its annual 
+      Missions Conference in Durame. This year we also hosted 100 university students and a few high school students that are 
+      very interested in serving the Lord as missionaries. In total 250 attended the conference. The conference was led by Dr. Desta 
+      and Justin Slemp. Justin is a bord member from AIPM USA. The following is the report from both Dr. Desta and Justin.`,
+      imageUrl: `${newsImageFive}`,
+    },
   ];
 
   const fetchPrayerMovement = async () => {
