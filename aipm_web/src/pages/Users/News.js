9312b083f0b1ba2b004437ff4afa6baadae8cfb7
eyosidesta <mamesmsdesta@gmail.com>
2024-03-15 22:54:18 -0400

    image val updated

--- aipm_web/src/pages/Users/News.js
+++ aipm_web/src/pages/Users/News.js
@@ -81,7 +81,7 @@ const News = () => {
           The following is the report from both Dr. Desta and Justin. During the 2nd week of October 2021, AIPM held its annual 
           Missions Conference in Durame. This year we also hosted 100 university students and a few high school students that are 
           very interested in serving the Lord as missionaries. In total 250 attended the conference.`,
-      imageUrl: `${newsImageThree}`,
+      imageUrl: `${newsImageTwo}`,
     },
     {
       title: "Nov 20, 2022 Missionary Training",
