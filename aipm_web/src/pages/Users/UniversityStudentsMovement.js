9606abb6e38c5d8e60acea3e0ff4ac07284fe16a
eyosidesta <mamesmsdesta@gmail.com>
2024-04-10 03:08:30 -0400

    Adowa University movement added

--- aipm_web/src/pages/Users/UniversityStudentsMovement.js
+++ aipm_web/src/pages/Users/UniversityStudentsMovement.js
@@ -435,6 +435,23 @@ const UniversityStudentsMovement = () => {
         The conference was led by Dr. Desta and Justin Slemp.
         and Justin.
       </Typography>
+      <Typography variant="h3" className={classes.title}>
+        Adowa University Students Movement
+      </Typography>
+      <Typography className={classes.description}>
+        During the 2nd week of October 2021, AIPM held its annual Missions
+        Conference in Durame. This year we also hosted 100 university students
+        and a few high school students that are very interested in serving the
+        Lord as missionaries. In total 250 attended the conference. The
+        conference was led by Dr. Desta and Justin Slemp. Justin is a bord
+        member from AIPM USA. The following is the report from both Dr. Desta
+        and Justin. During the 2nd week of October 2021, AIPM held its annual
+        Missions Conference in Durame. This year we also hosted 100 university
+        students and a few high school students that are very interested in
+        serving the Lord as missionaries. In total 250 attended the conference.
+        The conference was led by Dr. Desta and Justin Slemp.
+        and Justin.
+      </Typography>
       {isLoading ? (
         <div className={classes.circularProgress}>
           <CircularProgress />
